f9a05c9695bd8e2a5e7e167b5d09b758ffe44523
codesmitty <varcodesmitty@gmail.com>
2022-04-06 08:24:11 -0400

    testing netlify

---
+++ studio/schemas/headerImages.js
@@ -0,0 +1,25 @@
+export default {
+  name: "images",
+  type: "array",
+  title: "Images",
+  of: [
+    {
+      name: "image",
+      type: "image",
+      title: "Image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative text",
+        },
+      ],
+    },
+  ],
+  options: {
+    layout: "grid",
+  },
+};
