1245a46aad072b2c46faad027bbe83018e9d1f4c
codesmitty <varcodesmitty@gmail.com>
2022-04-29 12:51:27 -0400

    changes to sanity config

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,20 +1,33 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+console.log(process.env.SANITY_PROJECT_ID)
 module.exports = {
   siteMetadata: {
     title: `cbtavern`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-sanity',
-    options: {
-      "projectId": "t0wrjvht",
-      "dataset": "production"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: "production",
+        token: process.env.SANITY_TOKEN,
+        watchMode: true,
+        overlayDrafts: true,
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,20 +1,22 @@
 import * as React from "react";
+import { useEffect } from "react";
 import { graphql } from "gatsby";
 import "bootstrap/dist/css/bootstrap.min.css";
 // component imports
 import Layout from "../components/layout/Layout.js";
 import HeaderCorousel from "../components/headerCorousel/headerCorousel.js";
 import Homepage from "./Homepage.js";
-const IndexPage = ({data}) => {
-  
-  return <Layout data={data}>
-    <HeaderCorousel data={data} />
-    <Homepage />
-     </Layout>;
+
+const IndexPage = ({ data }) => {
+  return (
+    <Layout data={data}>
+      <HeaderCorousel data={data} />
+      <Homepage />
+    </Layout>
+  );
 };
 
 export default IndexPage;
-
 
 export const query = graphql`
   {
@@ -23,7 +25,7 @@ export const query = graphql`
         display
         images {
           asset {
-            gatsbyImageData(layout: FULL_WIDTH, height:600)
+            gatsbyImageData(layout: FULL_WIDTH, height: 600)
           }
           imageTitle
         }
