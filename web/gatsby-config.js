c5d28e8d9ab2233e432eab4f9d245c92a1ce3f60
codesmitty <varcodesmitty@gmail.com>
2022-04-29 13:15:56 -0400

    changes to sanity config

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     {
       resolve: "gatsby-source-sanity",
       options: {
-        projectId: process.env.SANITY_PROJECT_ID,
+        projectId: "t0wrjvht",
         dataset: "production",
         token: process.env.SANITY_TOKEN,
         watchMode: true,
