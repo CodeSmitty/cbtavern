07ee8d02f4d0cf566def43116c7b18e52b802afd
codesmitty <varcodesmitty@gmail.com>
2022-05-03 08:35:54 -0400

    styling menu

--- web/src/components/menuItem/MenuItem.js
+++ web/src/components/menuItem/MenuItem.js
@@ -9,11 +9,19 @@ function MenuItem({item, category}) {
         {item?.map((menuItem, i)=>{ return (
             
           <div className="menuItem-tabs" key={i}>
-                <h3>{menuItem.title}</h3>
-                <p>${menuItem?.price}</p>
-                {menuItem?._rawMealItemDescription ? <PortableText
-                  blocks={menuItem?._rawMealItemDescription}
-                />:null}
+                <div className="menu-item-content-wrapper">
+                  <h3>{menuItem.title}</h3>
+                  <p className="menu-item-price">${menuItem?.price}</p>
+                </div>
+                <hr />
+                <div>
+                  
+                  {menuItem?._rawMealItemDescription ? <PortableText
+                    blocks={menuItem?._rawMealItemDescription}
+
+                    className="menu-item-description"
+                  />:null}
+                </div>
             
            
           </div>
